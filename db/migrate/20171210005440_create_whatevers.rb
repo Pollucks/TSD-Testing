2f9e8ebf318ad420bc58eb3df67f8fcbcb44d6e3
Tristan Allan <tallan@uncc.edu>
2017-12-10 00:55:34 +0000

    Added the model

---
+++ db/migrate/20171210005440_create_whatevers.rb
@@ -0,0 +1,20 @@
+class CreateWhatevers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :whatevers do |t|
+      t.string :name
+      t.text :background
+      t.string :race
+      t.string :classes
+      t.integer :strength
+      t.integer :dexterity
+      t.integer :constitution
+      t.integer :intelligence
+      t.integer :wisdom
+      t.integer :charisma
+      t.string :armorclass
+      t.integer :gold
+
+      t.timestamps
+    end
+  end
+end
