1f4e35d74017d2a49d08cae8e33aac2f7f2dc51a
aashfor1 <aashfor1@uncc.edu>
2017-12-10 02:28:38 +0000

    Made new views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'characters/index'
+    resources :characters
   root 'characters#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :characters
 end
